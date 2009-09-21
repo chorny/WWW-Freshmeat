db4a49b64769c9e7955a610f2490cc921fd76d02
Alexandr Ciornii <alexchorny@gmail.com>
2009-09-21 15:43:38 +0300

    use Mouse

--- lib/WWW/Freshmeat/Project/URL.pm
+++ lib/WWW/Freshmeat/Project/URL.pm
@@ -1,3 +1,11 @@
 package WWW::Freshmeat::Project::URL;
+use Mouse;
+has 'url' => (is => 'rw', isa => 'Str', 'builder'=>'find_url','lazy'=>1);
+
+no Mouse;
+
+sub find_url {
+  my $self=shift || die;
+}
 
 1;

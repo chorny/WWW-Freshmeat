265519ea831173ca4df47a0a876acb026a40e536
Alexandr Ciornii <alexchorny@gmail.com>
2009-09-21 16:27:33 +0300

    now do it really

--- lib/WWW/Freshmeat/Project/URL.pm
+++ lib/WWW/Freshmeat/Project/URL.pm
@@ -1,7 +1,7 @@
 package WWW::Freshmeat::Project::URL;
 use Mouse;
 use Carp;
-has 'url' => (is => 'rw', isa => 'Str', 'builder'=>'_build_url','lazy'=>1);
+has 'url' => (is => 'rw', isa => 'Str', 'builder'=>'_find_url','lazy'=>1);
 has 'label' => (is => 'rw', isa => 'Str',required=>1);
 has 'redirector' => (is => 'rw', isa => 'Str');
 has 'host' => (is => 'rw', isa => 'Str');
@@ -9,7 +9,7 @@ has 'www_freshmeat' => (is => 'rw', isa => 'WWW::Freshmeat',required=>1);
 
 no Mouse;
 
-sub _build_url {
+sub _find_url {
   my $self=shift || die;
   croak "No 'redirector' field" unless $self->redirector;
   return $self->www_freshmeat->redir_url($self->redirector);

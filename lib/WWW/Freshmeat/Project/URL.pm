d424172d412b702a2b708ca697b69724332ab514
Alexandr Ciornii <alexchorny@gmail.com>
2009-09-21 15:48:11 +0300

    define class data

--- lib/WWW/Freshmeat/Project/URL.pm
+++ lib/WWW/Freshmeat/Project/URL.pm
@@ -1,6 +1,10 @@
 package WWW::Freshmeat::Project::URL;
 use Mouse;
 has 'url' => (is => 'rw', isa => 'Str', 'builder'=>'find_url','lazy'=>1);
+has 'label' => (is => 'rw', isa => 'Str',required=>1);
+has 'redirector' => (is => 'rw', isa => 'Str');
+has 'host' => (is => 'rw', isa => 'Str');
+has 'www_freshmeat' => (is => 'rw', isa => 'WWW::Freshmeat',required=>1);
 
 no Mouse;
 
